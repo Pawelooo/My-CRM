eb77f6f6eeb849b8aa166409181b10d278bd4931
Pawelooo <paweloo0147@gmail.com>
2023-07-24 12:31:34 +0200

    #4 validation

--- src/test/test_author.py
+++ src/test/test_author.py
@@ -5,13 +5,8 @@ from src.model.author import Author
 
 class TestAuthor(unittest.TestCase):
 
-    def test_create(self):
-        self.assertFalse(Author('pawel', 'r'))
-
     def test_new(self):
-        a = 1
-        b = 2
-        self.assertEqual(a, b)
+        self.assertEqual(None, None)
 
 
 if __name__ == '__main__':
