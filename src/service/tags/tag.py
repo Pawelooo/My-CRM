1a4daa51b7ef1c5f5cba93e35afe5fe7de948d6e
Pawelooo <paweloo0147@gmail.com>
2023-07-26 12:50:26 +0200

    #23 tag mechanism

--- src/service/tags/tag.py
+++ src/service/tags/tag.py
@@ -4,10 +4,12 @@ from src.model.config import FILE_ENCODING, FILE_TAG, FILE_LOCATION_TAG
 
 
 class Tag:
-    def __init__(self):
+    def __init__(self, file_name: str):
+        self.file_name = file_name
         self.values = []  # the db should be updated accordingly
 
     def find_all(self):
+        self.get_all()
         return self.values
 
     def get(self, value):
@@ -25,12 +27,12 @@ class Tag:
         return self.get(value)
 
     def get_all(self):
-        with open(f'{FILE_LOCATION_TAG}{FILE_TAG}', 'r+',
+        with open(f'{FILE_LOCATION_TAG}{self.file_name}', 'r+',
                   encoding=FILE_ENCODING) as f:
             self.values = json.load(f)
 
     def save(self):
-        with open(f'{FILE_LOCATION_TAG}{FILE_TAG}', 'w+',
+        with open(f'{FILE_LOCATION_TAG}{self.file_name}', 'w+',
                   encoding=FILE_ENCODING) as file:
             json.dump(self.values, file)
 
