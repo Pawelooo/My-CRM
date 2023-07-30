2268d4abd66cdb04b0b984f775f1c0ee5ebeb8e9
Pawelooo <paweloo0147@gmail.com>
2023-07-30 17:01:17 +0200

    #25 finding items from the roadmap

--- src/model/respository.py
+++ src/model/respository.py
@@ -1,7 +1,8 @@
 import json
 
 from src.model.author import Author
-from src.model.config import FILE_USER_NAME, FILE_LOCATION, FILE_ENCODING, FILE_AUTHOR_NAME, IDENTIFIER
+from src.model.config import FILE_USER_NAME, FILE_LOCATION, FILE_ENCODING, \
+    FILE_AUTHOR_NAME, IDENTIFIER
 
 
 class Repository:
@@ -35,6 +36,18 @@ class Repository:
                 return json.load(f)
             except:
                 return []
+
+    def find_limit(self, file_path: str, limit: int, priority: str):
+        lst = []
+        all_objs = self.find_all(file_path)
+        for obj in all_objs:
+            if obj['priority'].lower() == priority.lower():
+                print('tak')
+                lst.append(obj)
+            if limit == len(lst):
+                break
+
+        return lst
 
     def save_all(self, file_path: str, objects: list[Author]):
         with open(file_path, 'w+', encoding=FILE_ENCODING) as f:
