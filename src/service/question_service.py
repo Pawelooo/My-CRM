d69d14be04acb9374da04baf73cbc80f70cc1009
Pawelooo <paweloo0147@gmail.com>
2023-08-05 15:32:27 +0200

    #37 question random modes

--- src/service/question_service.py
+++ src/service/question_service.py
@@ -13,10 +13,11 @@ class QuestionService:
 
     def get_unique_question(self):
         questions = self.repository.find_all(f'{FILE_LOCATION}{FILE_QUESTION}')
-        questions = [question for question in questions if question not in
-                     self.used_questions]
-        while questions:
-            rd_question = random.choice(questions)
+        question_not_used = [question for question in questions if
+                             question not in
+                             self.used_questions]
+        while question_not_used:
+            rd_question = random.choice(question_not_used)
             if rd_question not in self.used_questions:
                 self.used_questions.append(rd_question)
                 return rd_question
