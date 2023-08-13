e33fceaf77d3d6a4e4531bf579fdeba50c2f6c1f
Pawelooo <paweloo0147@gmail.com>
2023-08-13 12:49:51 +0200

    #47 remote files storage crud

---
+++ src/service/jfs.py
@@ -0,0 +1,36 @@
+import json
+from ast import literal_eval
+import requests
+
+from src.model.config import FILE_LOCATION, FILE_QUESTION, FILE_ROADMAP
+
+
+class JsonFromService:
+
+    def __init__(self):
+        self.link = 'http://89.74.210.195:8090/jfs-cloud-app-ci/v1/api/storage/'
+        self.headers = {'Tenant-Id': 'My-CRM'}
+
+    def add_file(self, name_file: str, type_s: str):
+        files = {'file': open(f'{FILE_LOCATION}{name_file}', 'rb')}
+        return requests.post(f'{self.link}{type_s}', headers=self.headers, files=files).status_code
+
+    def update_file(self, name_file: str, type_s: str):
+        files = {'file': open(f'{FILE_LOCATION}{name_file}', 'rb')}
+        return requests.post(f'{self.link}{type_s}', headers=self.headers, files=files).status_code
+
+    def read_file(self, name_file: str, type_s: str):
+        response = requests.get(f'{self.link}{type_s}?filename={name_file}', headers=self.headers)
+        response_d = literal_eval(response.content.decode('utf-8'))
+        with open(f'{FILE_LOCATION}{name_file}', 'w') as file:
+            json.dump(response_d, file)
+        return response_d
+
+    def get_objects_with_parametrization(self, name_file: str, type_s: str,
+                                         parametrization: str):
+        response = requests.get(f'{self.link}{type_s}?filename={name_file}',
+                                headers=self.headers)
+        response_con = literal_eval(response.content.decode('utf-8'))
+        return [obj for obj in response_con if parametrization in obj.values()]
+
+
