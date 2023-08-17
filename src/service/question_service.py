268c931732ba831590076a670502b48e6df24059
Pawelooo <paweloo0147@gmail.com>
2023-08-17 20:53:21 +0200

    #48-localremote-files-storages-parametrization

--- src/service/question_service.py
+++ src/service/question_service.py
@@ -1,13 +1,22 @@
 import random
+<<<<<<< HEAD
 from src.model.config import FILE_LOCATION, FILE_QUESTION
 from src.model.question import Question
 from src.model.respository import Repository
+=======
+
+from src.model.config import FILE_LOCATION, FILE_QUESTION, FILE_TAG
+from src.model.question import Question
+from src.model.respository import Repository
+from src.service.tags.tag import Tag
+>>>>>>> 5cba2b3 (#38 question random parametriazation)
 
 
 class QuestionService:
 
     def __init__(self):
         self.repository = Repository()
+<<<<<<< HEAD
         self.used_questions = []
 
     def create(self, obj: Question):
@@ -40,4 +49,6 @@ class QuestionService:
         questions_all = self.repository.find_all(
             f'{FILE_LOCATION}{FILE_QUESTION}')
         return random.choice([question for question in questions_all if question['Tag'] == parametrization])
+
+
 
