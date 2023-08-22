5e74f606094da920e4d551208fb2b0f77ced1483
Pawelooo <paweloo0147@gmail.com>
2023-08-22 19:41:36 +0200

    #49 upload validation

--- src/service/validators/jfs_validation.py
+++ src/service/validators/jfs_validation.py
@@ -1,4 +1,6 @@
 from ast import literal_eval
+
+from src.model.config import CONTENT_ENCODE
 from src.model.validator import Validator
 
 
@@ -8,7 +10,7 @@ class JfsValidator(Validator):
         result_status = result.status_code
         if 200 >= result_status < 300:
             return None
-        result_content = literal_eval(result.content.decode('utf-8'))['messages']
+        result_content = literal_eval(result.content.decode(CONTENT_ENCODE))['messages']
         if 400 > result_status < 500:
             return self.create_validation(result_content)
         if result_status >= 500:
