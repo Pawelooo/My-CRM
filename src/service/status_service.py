62f8ab8c6f23f2f54f185ff553c6f1a535d6ac91
Pawelooo <paweloo0147@gmail.com>
2023-08-30 15:32:41 +0200

    #62 items statuses

--- src/service/status_service.py
+++ src/service/status_service.py
@@ -12,7 +12,7 @@ class StatusService:
         self.repository.create(f'{FILE_LOCATION}{FILE_STATUS_NAME}', obj)
 
     def read(self, ):
-        return self.repository.find_all(f'{FILE_LOCATION}{FILE_AUTHOR_NAME}')
+        return self.repository.find_all(f'{FILE_LOCATION}{FILE_STATUS_NAME}')
 
     def update(self, obj: Status, key: str):
         self.repository.update(f'{FILE_LOCATION}{FILE_STATUS_NAME}', obj, key)
