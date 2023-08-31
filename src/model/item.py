df5c4fbdd927070c38a9d2c963c8c96d7d3cd120
Pawelooo <paweloo0147@gmail.com>
2023-08-31 19:40:46 +0200

    #62 items statuses

--- src/model/item.py
+++ src/model/item.py
@@ -32,6 +32,7 @@ class Item:
             'deadline': self.deadline,
             'category': self.category,
             'assignee': self.assignee,
+            'status': self.status
         }
 
     @staticmethod
@@ -43,4 +44,3 @@ class Item:
         self.status = obj[self.level]
         if not self.level >= len(obj) - 1:
             self.level += 1
-
