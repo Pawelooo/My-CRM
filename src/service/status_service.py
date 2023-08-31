015e481fb7727969aceb96e7c553b06d28ccb09b
Pawelooo <paweloo0147@gmail.com>
2023-08-31 19:26:15 +0200

    #61 subitems statuses

--- src/service/status_service.py
+++ src/service/status_service.py
@@ -1,4 +1,4 @@
-from src.model.config import FILE_LOCATION, FILE_STATUS_NAME, FILE_AUTHOR_NAME
+from src.model.config import FILE_LOCATION, FILE_STATUS_NAME
 from src.model.respository import Repository
 from src.model.status import Status
 
@@ -12,7 +12,7 @@ class StatusService:
         self.repository.create(f'{FILE_LOCATION}{FILE_STATUS_NAME}', obj)
 
     def read(self, ):
-        return self.repository.find_all(f'{FILE_LOCATION}{FILE_AUTHOR_NAME}')
+        return self.repository.find_all(f'{FILE_LOCATION}{FILE_STATUS_NAME}')
 
     def update(self, obj: Status, key: str):
         self.repository.update(f'{FILE_LOCATION}{FILE_STATUS_NAME}', obj, key)
