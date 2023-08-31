20ad840837ca070c967daab1fbae1cbbcd0560c9
Pawelooo <paweloo0147@gmail.com>
2023-08-31 19:31:42 +0200

    #61 subitems statuses

--- src/model/subitem.py
+++ src/model/subitem.py
@@ -1,4 +1,5 @@
 from src.model.generator import Generator
+from src.service.status_service import StatusService
 
 
 class SubItem:
@@ -12,6 +13,10 @@ class SubItem:
         self.opened_by = None
         self.deadline = None
         self.done = False
+        self.status_opt = StatusService()
+        self.level = 0
+        self.status = None
+        self.update_status()
 
     def __repr__(self):
         return {
@@ -22,6 +27,12 @@ class SubItem:
             'id item': self.id_item,
             'opened by': self.opened_by,
             'deadline': self.deadline,
-            'done': self.done
+            'done': self.done,
+            'status': self.status
         }
 
+    def update_status(self):
+        obj = list(self.status_opt.read()[0].values())
+        self.status = obj[self.level]
+        if not self.level >= len(obj) - 1:
+            self.level += 1
