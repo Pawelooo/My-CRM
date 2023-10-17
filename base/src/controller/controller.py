b0fa2d8bbb7d5cbf66c384013a59b0c96d5191a6
Pawelooo <paweloo0147@gmail.com>
2023-10-17 19:42:45 +0200

    #96 module 1 basic crud rest api

--- base/src/controller/controller.py
+++ base/src/controller/controller.py
@@ -1,14 +1,6 @@
 import logging
 
 from base.src.model.model import Model
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-from base.src.model.test import Test
->>>>>>> dd4209d (#96 module 1 basic crud rest api)
-=======
-from base.src.model.test import Test
->>>>>>> dd4209d12a5419d449005abe09a2927b86221da9
 from base.src.view.view import View
 
 
